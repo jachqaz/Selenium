08b31de9a4784e8c53c2a4b83cda8d0df32289a3
Johan Castano <johan.castano@arkangroupco.com>
2020-02-10 17:43:32 -0500

    44. Sample code for download explaining xpath text/siblings/parent

--- src/com/company/testit.java
+++ src/com/company/testit.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class testit {
     public static void main(String[] args) throws InterruptedException {
-// TODO Auto-generated method stub
         System.setProperty("webdriver.chrome.driver", "/home/rodolhan/Descargas/chromedriver_linux64/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("http://www.qaclickacademy.com/interview.php");
@@ -15,6 +14,5 @@ public class testit {
         driver.findElement(By.xpath("//ul[@class='responsive-tabs__list']/li[1]/following-sibling::li[2]")).click();
 
         System.out.println(driver.findElement(By.xpath(".//*[@id='tablist1-tab2']/parent::ul")).getAttribute("role"));
-//
     }
 }
