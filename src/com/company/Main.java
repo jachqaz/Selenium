3dea0541ea0550bee3419935ec68e3ba757a5dae
Johan Castano <johan.castano@arkangroupco.com>
2020-02-10 11:56:41 -0500

    27. Importance of Locator Identifiers in Selenium

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,15 +10,18 @@ public class Main {
         // write your code here
         System.setProperty("webdriver.chrome.driver", "/home/rodolhan/Descargas/chromedriver_linux64/chromedriver");
         WebDriver driver = new ChromeDriver();
-        driver.get("http://google.com");
-        System.out.println(driver.getTitle());
-        System.out.println(driver.getCurrentUrl());
+//        driver.get("http://google.com");
+//        System.out.println(driver.getTitle());
+//        System.out.println(driver.getCurrentUrl());
         //System.out.println(driver.getPageSource());
-        driver.get("http://yahoo.com/");
+//        driver.get("http://yahoo.com/");
 //        driver.navigate().back();
 //        driver.navigate().forward();
-        driver.close();
+//        driver.close();
 //        driver.quit();
+
+        driver.get("http://facebook.com");
+        driver.findElement(By.id("email")).sendKeys("jach_1997@hotmail.com");
 
     }
 }
